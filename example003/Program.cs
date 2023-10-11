48d67cfeaa56c1ee3256cf6f8393c56487e1899c
nikita <staffololo@gmail.com>
2023-10-11 15:21:52 +0500

    random

--- example003/Program.cs
+++ example003/Program.cs
@@ -1,5 +1,7 @@
-﻿int numberA = 7;
-int numberB = 17;
+﻿int numberA = new Random().Next(1, 10); // случайное число от 1 до 9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
 int result = (numberA + numberB);
 Console.WriteLine(result);
 

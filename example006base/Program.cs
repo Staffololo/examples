abd035d4b1f1c0652731a139680a72895f8e8bde
nikita <staffololo@gmail.com>
2023-10-11 15:56:30 +0500

    base

---
+++ example006base/Program.cs
@@ -0,0 +1,12 @@
+﻿int a = 3;
+int b = 11;
+int c = 7;
+int d = 5;
+int e = 2;
+int max = a;
+if (a > max ) max = a;
+if (b > max ) max = b;
+if (c > max ) max = c;
+if (d > max ) max = d;
+if (e > max ) max = e;
+Console.Write( max );
